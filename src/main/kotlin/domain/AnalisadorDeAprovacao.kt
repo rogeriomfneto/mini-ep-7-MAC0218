a29b0e73299c59f403fdbc0fc8f526e2a8fbde52
Rogerio Fernandes <rogerio.fernandes@wildlifestudios.com>
2022-05-15 14:23:13 -0300

    feat: implement AnalisadorDeAprovacao according to tests

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,21 @@
 package domain
 
+import domain.criterios.Basico
+import domain.criterios.CriterioDeAprovacao
+
 class AnalisadorDeAprovacao {
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+   private var criterio: CriterioDeAprovacao = Basico();
+    fun defineCriterio(criterio: CriterioDeAprovacao) {
+        this.criterio = criterio;
+    }
 
+    fun fechaBoletim(boletim: Boletim): BoletimFechado {
+        return BoletimFechado(
+            boletim.mediaEPs,
+            boletim.mediaMiniEPs,
+            this.criterio.mediaFinal(boletim),
+            this.criterio.estaAprovado(boletim)
+        );
+    }
 }
